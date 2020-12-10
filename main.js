19330686495e4c3f33598ea8a25eb7e3279b409c
kajihirot <hiroto.kaji0@gmail.com>
2020-12-11 03:55:57 +0900

    aaa

--- main.js
+++ main.js
@@ -45,3 +45,81 @@ $(function () {
         }
     });
 });
+
+
+const carouselWidth = $("#carousel li").width();
+const length = $(".carousel-item").length;
+const innerWidth = carouselWidth * length;
+const time = 400;
+
+const carouselinner = $("#carousel");
+const prev = $("#prev");
+const next = $("#next");
+
+let c = 1;
+
+next.click(function () {
+    if (c == length) {
+        carouselinner.stop().animate({
+                left: 0,
+
+            },
+            time
+        );
+        c = 1;
+    } else {
+        carouselinner.stop().animate({
+                left: -c * carouselWidth,
+            },
+            time
+        );
+        c++;
+    }
+});
+
+prev.click(function () {
+    if (c == 1) {
+        carouselinner.stop().animate({
+                left: carouselWidth - innerWidth,
+            },
+            time
+        );
+        c = length;
+
+    } else {
+        carouselinner.stop().animate({
+                left: -(c - 2) * carouselWidth,
+            },
+            time
+        );
+        c--;
+    }
+});
+
+$('.fa-bars').on('click', function () {
+    $('.nav').toggleClass('show');
+});
+
+//click イベントで発火
+document.body.addEventListener("click", drop, false);
+
+function drop(e) {
+
+    //座標の取得
+    var x = e.pageX;
+    var y = e.pageY;
+
+    //しずくになるdivの生成、座標の設定
+    var sizuku = document.createElement("div");
+    sizuku.style.top = y + "px";
+    sizuku.style.left = x + "px";
+    document.body.appendChild(sizuku);
+
+    //アニメーションをする className を付ける
+    sizuku.className = "sizuku";
+
+    //アニメーションが終わった事を感知してしずくを remove する
+    sizuku.addEventListener("animationend", function () {
+        this.parentNode.removeChild(this);
+    }, false);
+}
